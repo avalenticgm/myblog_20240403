2b5e811a8fea1445a0c9fd250b1777e584fa18f8
unknown <a.valenti@cgmconsulting.it>
2024-04-12 12:52:38 +0200

    Tag management

--- src/main/java/it/cgmconsulting/myblog/repository/TagRepository.java
+++ src/main/java/it/cgmconsulting/myblog/repository/TagRepository.java
@@ -12,5 +12,10 @@ public interface TagRepository extends JpaRepository<Tag, Short> {
     List<Tag> findByVisibleFalseOrderByTagName(); // select * from tag where visible = false order by tag_name asc
     List<Tag> findAllByOrderByTagName(); // select * from tag order by tag_name asc
 
+    boolean existsByTagName(String tagName);
+    boolean existsByTagNameAndIdNot(String newTagName, short id);
+
+    Optional<Tag> findByTagName(String tagName);
+
     Optional<Tag> findByTagNameAndVisibleTrue(String tagName); // select * from tag where tag_name= 'xxxx' AND visible = true
 }
