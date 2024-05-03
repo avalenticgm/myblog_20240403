397e016c3ac41bc3349748355b9edd02d218ddae
mygoo.run <mygoo.run@gmail.com>
2024-05-03 11:19:59 +0200

    get post

--- src/main/java/it/cgmconsulting/myblog/service/PostService.java
+++ src/main/java/it/cgmconsulting/myblog/service/PostService.java
@@ -2,21 +2,61 @@ package it.cgmconsulting.myblog.service;
 
 import it.cgmconsulting.myblog.entity.Post;
 import it.cgmconsulting.myblog.entity.User;
+import it.cgmconsulting.myblog.exception.ResourceNotFoundException;
 import it.cgmconsulting.myblog.payload.request.PostRequest;
+import it.cgmconsulting.myblog.payload.response.PostDetailResponse;
+import it.cgmconsulting.myblog.payload.response.PostResponse;
 import it.cgmconsulting.myblog.repository.PostRepository;
+import it.cgmconsulting.myblog.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
 
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
     public String  createPost(PostRequest request, UserDetails userDetails){
         Post post = new Post(request.getTitle(), request.getContent(), (User) userDetails);
         postRepository.save(post);
         return "New post having title "+post.getTitle()+" has been created";
+    }
+
+    public String editPost(int id, PostRequest request, UserDetails userDetails){
+        User user = (User) userDetails;
+        Post post = postRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Post", "Id", id));
+        boolean isAdmin = user.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .anyMatch("ADMIN"::equals);
+
+        if( user.getId() == post.getUserId().getId() || isAdmin ){
+            post.setTitle(request.getTitle());
+            post.setContent(request.getContent());
+            post.setUpdatedAt(LocalDateTime.now());
+            post.setPublicationDate(null);
+            postRepository.save(post);
+
+            return "Post edited successfully";
+        }
+        else return null;
+    }
+
+    public PostDetailResponse getPost(int id){
+        PostDetailResponse postResponse = postRepository.getPostById(id, LocalDate.now()).orElseThrow(
+                () -> new ResourceNotFoundException("Post", "Id", id));
+        return postResponse;
+    }
+
+    public List<PostResponse> getAllVisiblePosts() {
+        return postRepository.getVisiblePosts(LocalDate.now());
     }
 }
